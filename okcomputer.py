2cda0e570b24903eb7b27e2f44a1baf8ceb253b3
Michael Poluektov <michael.poluektov@gmail.com>
2025-11-29 12:56:03 +0000

    more demos

---
+++ okcomputer.py
@@ -0,0 +1,1 @@
+from test_genai_4 import ComputerUseAgent
---
+++ test_live_audio_2.py
@@ -0,0 +1,50 @@
+import asyncio
+import wave
+from google import genai
+from google.genai import types
+
+client = genai.Client()
+
+model = "gemini-2.5-flash-native-audio-preview-09-2025"
+
+# Simple function definitions
+turn_on_the_lights = {"name": "turn_on_the_lights"}
+turn_off_the_lights = {"name": "turn_off_the_lights"}
+
+tools = [{"function_declarations": [turn_on_the_lights, turn_off_the_lights]}]
+config = {"response_modalities": ["AUDIO"], "tools": tools}
+
+
+async def main():
+    async with client.aio.live.connect(model=model, config=config) as session:
+        prompt = "Turn on the lights please"
+        await session.send_client_content(turns={"parts": [{"text": prompt}]})
+
+        wf = wave.open("audio.wav", "wb")
+        wf.setnchannels(1)
+        wf.setsampwidth(2)
+        wf.setframerate(24000)  # Output is 24kHz
+
+        async for response in session.receive():
+            if response.data is not None:
+                wf.writeframes(response.data)
+            elif response.tool_call:
+                print("The tool was called")
+                function_responses = []
+                for fc in response.tool_call.function_calls:
+                    function_response = types.FunctionResponse(
+                        id=fc.id,
+                        name=fc.name,
+                        response={
+                            "result": "ok"
+                        },  # simple, hard-coded function response
+                    )
+                    function_responses.append(function_response)
+
+                await session.send_tool_response(function_responses=function_responses)
+
+        wf.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
